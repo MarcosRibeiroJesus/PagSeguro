841e6d19c08e6441d1dd7f3f16de1d4ecaceb2f5
Marcos Ribeiro <marcoz.iu@gmail.com>
2019-08-28 04:45:44 -0300

    Controller comentado

---
+++ Libraries/Javascript.js
@@ -0,0 +1,15 @@
+var Root="http://"+document.location.hostname+"/";
+
+$('#BotaoPagamento').on('click',function(event){
+    event.preventDefault();
+
+    $.ajax({
+        url: Root+"Controllers/ControllerPagamentoDireto.php",
+        type: 'POST',
+        dataType:'html',
+        success:function(data){
+            $('#code').val(data);
+            $('#FormPagamento').submit();
+        }
+    });
+});
